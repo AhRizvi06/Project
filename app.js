9206629de8bab51391d3dcda47d237389c01decf
Ahmmed Rizvi <ahrizvi1028@gmail.com>
2025-10-28 22:19:02 +0600

    Add home route to redirect listing

--- app.js
+++ app.js
@@ -84,6 +84,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings"); // 👈 or send a welcome message
+});
+
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
 });
